6afa55fbbaf3030d249fccff10d9e1a86be82034
kyberpunk <xvh@seznam.cz>
2016-11-23 17:49:48 +0100

    packages dependencies

--- pneuservis-service/src/main/java/cz/fi/muni/pa165/pneuservis/service/facade/OrderFacadeImpl.java
+++ pneuservis-service/src/main/java/cz/fi/muni/pa165/pneuservis/service/facade/OrderFacadeImpl.java
@@ -1,7 +1,9 @@
 package cz.fi.muni.pa165.pneuservis.service.facade;
 
+import cz.fi.muni.pa165.pneuservis.facade.OrderFacade;
+
 /**
  * Created by vit.holasek on 23.11.2016.
  */
-public class OrderFacadeImpl {
+public class OrderFacadeImpl implements OrderFacade {
 }
