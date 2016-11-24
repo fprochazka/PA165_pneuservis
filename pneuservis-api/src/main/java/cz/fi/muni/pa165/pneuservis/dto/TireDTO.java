f78fa3a0c49d1be6aa96a1a9fabb92364c474b20
MatejSipka <sipka.matej@gmail.com>
2016-11-24 23:45:35 +0100

    TireDTO implementation

--- pneuservis-api/src/main/java/cz/fi/muni/pa165/pneuservis/dto/TireDTO.java
+++ pneuservis-api/src/main/java/cz/fi/muni/pa165/pneuservis/dto/TireDTO.java
@@ -8,7 +8,7 @@ package cz.fi.muni.pa165.pneuservis.dto;
 import cz.fi.muni.pa165.pneuservis.enums.TireManufacturer;
 import cz.fi.muni.pa165.pneuservis.enums.TireType;
 import java.math.BigDecimal;
-import javax.validation.constraints.NotNull;
+import java.util.Objects;
 
 /**
  *
@@ -16,31 +16,129 @@ import javax.validation.constraints.NotNull;
  */
 public class TireDTO {
 
-    @NotNull
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     private TireType type;
 
-    @NotNull
     private int catalogNumber;
 
-    @NotNull
     private int tireSize;
 
-    @NotNull
     private int profile;
 
-    @NotNull
     private int diameter;
 
-    @NotNull
     private TireManufacturer manufacturer;
 
-    public TireDTO(TireType type, int catalogNumber, int tireSize, int diameter, TireManufacturer manufacturer, BigDecimal price, String description, String typeOfVehicle) {
-//        super(price, description, typeOfVehicle);
+    public TireDTO(TireType type, int catalogNumber, int tireSize, int diameter,
+            TireManufacturer manufacturer, BigDecimal price, String description, String typeOfVehicle) {
         this.type = type;
         this.catalogNumber = catalogNumber;
         this.tireSize = tireSize;
         this.diameter = diameter;
         this.manufacturer = manufacturer;
+    }
+
+    public TireType getType() {
+        return type;
+    }
+
+    public void setType(TireType type) {
+        this.type = type;
+    }
+
+    public int getCatalogNumber() {
+        return catalogNumber;
+    }
+
+    public void setCatalogNumber(int catalogNumber) {
+        this.catalogNumber = catalogNumber;
+    }
+
+    public int getTireSize() {
+        return tireSize;
+    }
+
+    public void setTireSize(int tireSize) {
+        this.tireSize = tireSize;
+    }
+
+    public int getProfile() {
+        return profile;
+    }
+
+    public void setProfile(int profile) {
+        this.profile = profile;
+    }
+
+    public int getDiameter() {
+        return diameter;
+    }
+
+    public void setDiameter(int diameter) {
+        this.diameter = diameter;
+    }
+
+    public TireManufacturer getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(TireManufacturer manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + Objects.hashCode(this.id);
+        hash = 29 * hash + Objects.hashCode(this.type);
+        hash = 29 * hash + this.catalogNumber;
+        hash = 29 * hash + this.tireSize;
+        hash = 29 * hash + this.profile;
+        hash = 29 * hash + this.diameter;
+        hash = 29 * hash + Objects.hashCode(this.manufacturer);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final TireDTO other = (TireDTO) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (this.type != other.type) {
+            return false;
+        }
+        if (this.catalogNumber != other.catalogNumber) {
+            return false;
+        }
+        if (this.tireSize != other.tireSize) {
+            return false;
+        }
+        if (this.profile != other.profile) {
+            return false;
+        }
+        if (this.diameter != other.diameter) {
+            return false;
+        }
+        if (this.manufacturer != other.manufacturer) {
+            return false;
+        }
+        return true;
     }
 
 }
