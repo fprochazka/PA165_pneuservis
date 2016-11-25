5daa6478f774de9e27c6b2cf323b2683a923de28
matej.sipka <matej.sipka@ixperta.com>
2016-11-25 18:31:25 +0100

    TireFacade javadoc

--- pneuservis-api/src/main/java/cz/fi/muni/pa165/pneuservis/facade/TireFacade.java
+++ pneuservis-api/src/main/java/cz/fi/muni/pa165/pneuservis/facade/TireFacade.java
@@ -16,26 +16,84 @@ import java.util.List;
  */
 public interface TireFacade {
 
+    /**
+     * Creates a new Tire
+     *
+     * @param tire Tire to create
+     * @throws IllegalArgumentException
+     */
     void create(TireDTO tire) throws IllegalArgumentException;
 
+    /**
+     * Updates an existing Tire
+     *
+     * @param tire Tire to update
+     */
     void update(TireDTO tire);
 
+    /**
+     * Deletes an existing Tire
+     *
+     * @param tire Existing tire to delete
+     */
     void delete(TireDTO tire);
 
+    /**
+     * Finds a Tire according to its Id
+     *
+     * @param id a Tire Id
+     * @return Found Tire or null if not found
+     */
     TireDTO findById(Long id);
 
+    /**
+     * Finds all stored Tires
+     *
+     * @return List of tires found
+     */
     List<TireDTO> findAll();
 
+    /**
+     * Finds all tires according to a catalog number
+     *
+     * @param catalogNumber A number of good from the catalog
+     * @return List of found tires
+     */
     List<TireDTO> findByCatalogNumber(int catalogNumber);
 
+    /**
+     * Finds all tires according to a profile
+     * @param profile a Tire profile
+     * @return List of found tires
+     */
     List<TireDTO> findByProfile(int profile);
 
+    /**
+     * Finds all tires according to a diameter
+     * @param diameter a Tire diameter
+     * @return List of found tires
+     */
     List<TireDTO> findByDiameter(int diameter);
 
+    /**
+     * Finds all tires according to a size
+     * @param size a Tire size
+     * @return List of found tires
+     */
     List<TireDTO> findBySize(int size);
 
+    /**
+     * Finds all tires according to a type
+     * @param type a Tire type
+     * @return List of found tires
+     */
     List<TireDTO> findByType(TireType type);
 
+    /**
+     * Finds all tires according to a manufacturer
+     * @param manufacturer a Tire manufacturer
+     * @return List of found tires
+     */
     List<TireDTO> findByManufacturer(TireManufacturer manufacturer);
 
 }
