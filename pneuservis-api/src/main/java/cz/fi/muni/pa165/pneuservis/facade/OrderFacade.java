d04f48eecc24d1041d36ad663196118adb24b20d
kyberpunk <xvh@seznam.cz>
2016-11-25 22:10:59 +0100

    Order fasade javadoc

--- pneuservis-api/src/main/java/cz/fi/muni/pa165/pneuservis/facade/OrderFacade.java
+++ pneuservis-api/src/main/java/cz/fi/muni/pa165/pneuservis/facade/OrderFacade.java
@@ -13,24 +13,48 @@ import java.util.List;
 public interface OrderFacade {
 
     /**
-     * Create new order
-     * @param order Order
+     * Create new order.
+     * @param order Order to create
      * @return Order with generated ID
      */
     OrderDTO create(CreateOrderDTO order);
 
     /**
-     * @param order
+     * Update order data.
+     * @param order Order to update
      */
     void update(UpdateOrderDTO order);
 
+    /**
+     * Delete order.
+     * @param order
+     */
     void delete(OrderDTO order);
 
+    /**
+     * Find order with given ID.
+     * @param orderId Order ID
+     * @return Order
+     */
     OrderDTO findOrderById(Long orderId);
 
+    /**
+     * Find orders that belongs to the client with given ID
+     * @param clientId Client ID
+     * @return List of orders
+     */
     List<OrderDTO> findClientOrders(Long clientId);
 
+    /**
+     * Find all orders
+     * @return List of orders
+     */
     List<OrderDTO> findAllOrders();
 
+    /**
+     * Generate billing information of the order with given ID
+     * @param orderId Order ID
+     * @return Billing information
+     */
     OrderBillingDTO getOrderBilling(Long orderId);
 }
